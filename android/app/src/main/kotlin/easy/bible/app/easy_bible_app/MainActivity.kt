b3d53d7665f66a3c2abd61d76cfe7f4f0cfb5b2d
hmy <alsdud110@jness.co.kr>
2025-10-20 16:58:58 +0900

    안드로이드 15 경고 수정

--- android/app/src/main/kotlin/easy/bible/app/easy_bible_app/MainActivity.kt
+++ android/app/src/main/kotlin/easy/bible/app/easy_bible_app/MainActivity.kt
@@ -4,3 +4,23 @@ import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
 }
+package easy.bible.app.easy_bible_app
+
+import io.flutter.embedding.android.FlutterActivity
+import android.os.Build
+import android.os.Bundle
+
+class MainActivity: FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Android 15 대응
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.VANILLA_ICE_CREAM) {
+            enableEdgeToEdge()
+        }
+    }
+    
+    private fun enableEdgeToEdge() {
+        window.decorView.setOnApplyWindowInsetsListener(null)
+    }
+}
